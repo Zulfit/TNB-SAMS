e7b91fb0012117c579432b7db85bde93b351e0c6
Zulfit <zulfitrih@gmail.com>
2025-02-13 01:01:46 +0800

    push dashboard

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -8,65 +8,54 @@
             <i class="bi bi-grid"></i>
             <span>Dashboard</span>
         </a>
-        </li><!-- End Dashboard Nav -->
-
-        <li class="nav-item">
-            <a class="nav-link collapsed" data-bs-target="#staff-nav" data-bs-toggle="collapse" href="#">
-                <i class="bi bi-people"></i><span>Staff</span><i class="bi bi-chevron-down ms-auto"></i>
-            </a>
-            <ul id="staff-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
-                <li>
-                {{-- <a href="{{route('staff.index')}}"> --}}
-                    <i class="bi bi-circle"></i><span>Staff List</span>
-                </a>
-                </li>
-            </ul>
         </li>
 
         <li class="nav-item">
-        <a class="nav-link collapsed" data-bs-target="#academicican-nav" data-bs-toggle="collapse" href="#">
-            <i class="bi bi-person-lines-fill"></i><span>Academicians</span><i class="bi bi-chevron-down ms-auto"></i>
-        </a>
-        <ul id="academicican-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
-            <li>
-            {{-- <a href="{{route('academicians.index')}}"> --}}
-                <i class="bi bi-circle"></i><span>Academicians List</span>
+            <a class="nav-link collapsed" href="#">
+                <i class="bi bi-bar-chart"></i><span>Analytics</span>
             </a>
-            </li>
-        </ul>
-        </li><!-- End Components Nav -->
+        </li>
 
         <li class="nav-item">
-        <a class="nav-link collapsed" data-bs-target="#forms-nav" data-bs-toggle="collapse" href="#">
-            <i class="bi bi-clipboard-data"></i><span>Project</span><i class="bi bi-chevron-down ms-auto"></i>
-        </a>
-        <ul id="forms-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
-            <li>
-            {{-- <a href="{{route('projects.index')}}"> --}}
-                <i class="bi bi-circle"></i><span>Project List</span>
+            <a class="nav-link collapsed" href="#">
+                <i class="bi bi-database"></i><span>Dataset</span>
             </a>
-            </li>
-            <li>
-            {{-- <a href="{{route('projects.create')}}"> --}}
-                <i class="bi bi-circle"></i><span>Project Create</span>
-            </a>
-            </li>
-        </ul>
-        </li><!-- End Forms Nav -->
+        </li>
 
         <li class="nav-item">
-        <a class="nav-link collapsed" data-bs-target="#tables-nav" data-bs-toggle="collapse" href="#">
-            <i class="bi bi-check2-circle"></i><span>Milestone</span><i class="bi bi-chevron-down ms-auto"></i>
-        </a>
-        <ul id="tables-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
-            <li>
-            {{-- <a href="{{route('milestones.index')}}"> --}}
-                <i class="bi bi-circle"></i><span>Milestone List</span>
+            <a class="nav-link collapsed" href="#">
+                <i class="bi bi-lightning-charge"></i><span>Substation</span>
             </a>
-            </li>
-        </ul>
-        </li><!-- End Tables Nav -->
+        </li>
+
+        <li class="nav-item">
+            <a class="nav-link collapsed" href="#">
+                <i class="bi bi-box-seam"></i><span>Asset</span>
+            </a>
+        </li>
+
+        <li class="nav-item">
+            <a class="nav-link collapsed" href="#">
+                <i class="bi bi-cpu"></i><span>Sensor</span>
+            </a>
+        </li>
+
+        <li class="nav-item">
+            <a class="nav-link collapsed" href="#">
+                <i class="bi bi-clipboard-data"></i><span>Report Log</span>
+            </a>
+        </li>
+
+        <li class="nav-item">
+            <a class="nav-link collapsed" href="#">
+                <i class="bi bi-box-arrow-right"></i><span>Log Out</span>
+            </a>
+        </li>
 
     </ul>
 
-    </aside><!-- End Sidebar-->
+    <div class="d-flex align-items-center">
+        <img src="assets/img/logo_tnb.jpeg" alt="" style="width: 90%">
+    </div>
+
+</aside><!-- End Sidebar-->
